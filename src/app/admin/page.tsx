0d27801c069ec199aac915cd89a34001474dbdba
Kenyansa <kenyansajustine@gmail.com>
2024-04-02 00:20:21 +0300

    add card content

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,17 @@
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+
 export default function AdminDashboard(){
-return <h1>Utibu Hospital!</h1>
+return  (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Card>
+            <CardHeader>
+                <CardTitle>Sales</CardTitle>
+                <CardDescription>desc</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p>Text</p>
+            </CardContent>
+        </Card>
+    </div>
+)
 }
