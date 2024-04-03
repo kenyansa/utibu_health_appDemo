78883e7b7d93059e7549790c95d7d7a222147832
Kenyansa <kenyansajustine@gmail.com>
2024-04-03 10:13:22 +0300

    hash plain passwords

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,7 +19,7 @@ async function isAuthenticated(req: NextRequest) {
   const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
     .toString()
     .split(":")
-
+    
   return (
     username === process.env.ADMIN_USERNAME &&
     (await isValidPassword(
